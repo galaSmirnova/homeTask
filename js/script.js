e4d82d362bbf76fac8ecf66983c38aa19da40d39
Galina <gggh@>
2020-12-07 11:42:48 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector(".menu"),
+    menuItem = document.querySelectorAll(".menu-item"),
+    title = document.querySelector(".title"),
+    ask = document.getElementById("prompt"),
+    div = document.querySelectorAll("column"),
+    adv = document.getElementsByClassName("adv");
+
+
+
+
+menu.replaceChild(menuItem[2], menuItem[1]);
+
+let li = document.createElement("li");
+    
+
+menu.appendChild(li);
+li.classList.add('menu-item');
+li.textContent = "Пятый пункт";
+
+document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
+
+title.textContent = "Мы продаем только подлинную технику Apple";
+
+// let answ = prompt("Как вы относитесь к технике apple?");
+
+// ask.textContent = answ;
+
